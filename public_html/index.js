4cac07fbac1ceb9d5982a491e787cb5a2f362bec
Nick <nickgrodriguez@outlook.com>
2017-07-16 08:45:31 -0700

    Fixed some logical issues

--- public_html/index.js
+++ public_html/index.js
@@ -8,13 +8,18 @@
      };
 
 $(document).ready(function(){
-    
+   //by default will check user's location and use that to determine weather 
     getLocation();
+    
+    //variables used throughout the program
+    
     var lat;
     var long;
      today = new Date();
-      var hour = today.getHours();
+     var hour = today.getHours();
+     var flag;
     
+//uses the browser to get the user's location
 
    function getLocation() {
     if (navigator.geolocation) {
@@ -32,22 +37,38 @@ $(document).ready(function(){
         getWeather(long, lat);
 
     }
+    
+    //takes inputs of the user's longitute and latitiude and searches the weather api
        function getWeather(long, lat){ 
 
            $.ajax({
         
         type: "GET",
+        
+        //Weather API needs long and lat to complete a search
+        
         url:"http://api.openweathermap.org/data/2.5/weather?lat=" + lat.toString() + 
                 "&lon=" + long.toString() + "&APPID=7bafabe02755edbbbee1b9b6b1d0046c",
         
         success:function(data){
+            
+            //using our singleton class to store data about the weather
             $("#weatherforecast").html(data.weather[0].description);
            weatherInfo.status = data.weather[0].main;
            weatherInfo.kelvin = data.main.temp;
            weatherInfo.cel = weatherInfo.kelvin - 273.15;
            weatherInfo.far = (weatherInfo.cel *1.8) + 32;
-           console.log(weatherInfo.far);
-            $(".far").html(toFar(weatherInfo.far));
+           
+           //If the Weather type is Celsius (only happens if we switch between cities with C selected)
+           //Then it will return the new city's Celsius, else it will default to Far
+           
+           if(weatherInfo.type === "C"){
+                 toCelcius(weatherInfo.cel);
+           }else{
+            $("#temp").html(toFar(weatherInfo.far));
+           }
+           
+           //As long as their city isn't one of our selected ones, it will display the name
             if(data.name !=="Manhattan" && data.name!=="Los Angeles" && data.name !== "Chicago"){
             $("#city").html(data.name);
         }
@@ -59,6 +80,9 @@ $(document).ready(function(){
         }
     }); 
        }
+       
+       //base background changing scheme, takes the weather and the hour to pick a night or day backgroung
+       //Also might change the color schemes
        function changeBG(weather, hour){
 
            switch(weather){
@@ -79,6 +103,7 @@ $(document).ready(function(){
        }
    }
    
+   //special functions for NY, LA, and Chicago
           function changeBGNY(hours){
                        if(hours < 19 && hours > 5){
                               $("body").css("background-image","url(https://images.unsplash.com/30/ny-filtered.jpg)");
@@ -94,7 +119,7 @@ $(document).ready(function(){
                  function changeBGLA(hours){
                        if(hours < 19 && hours > 5){
                               $("body").css("background-image","url(http://bit.ly/2tfBpyB)");
-                              $(".main-area").css("background-color","rgba(47, 45, 44, 0.5)");
+                              $(".main-area").css("background-color","rgba(66, 84, 17, 0.5)");
                               //$(".main-area").css("color","#a8bcdb");  
                           }else{
                               $("body").css("background-image","url(http://eskipaper.com/images/los-angeles-night-1.jpg)");
@@ -106,7 +131,7 @@ $(document).ready(function(){
                         function changeBGChi(hours){
                        if(hours < 19 && hours > 5){
                               $("body").css("background-image","url(http://bit.ly/2tY8bkQ)");
-                              $(".main-area").css("background-color","rgba(47, 45, 44, 0.5)");
+                              $(".main-area").css("background-color","rgba(226, 188, 160, 0.5)");
                               //$(".main-area").css("color","#a8bcdb");  
                           }else{
                               $("body").css("background-image","url(http://bit.ly/2tfqNzT)");
@@ -114,28 +139,32 @@ $(document).ready(function(){
                               
                           }
        }
-       
+   
+   //Outputs Fahrenheit data
    function toFar(far){
        $("#temp").html(far.toFixed(2));
        weatherInfo.type = "F";
-       $("#tempToggle").css("value", "To Celsuis");
+       $("#tempType").html(weatherInfo.type);
+       
    }
    function toCelcius(cel){
        $("#temp").html(cel.toFixed(2));
        weatherInfo.type = "C";
-       $("#fOrC").html(weatherInfo.type);
+       $("#tempType").html(weatherInfo.type);
    }
    
+   
+   //Controls the C/F button actions
    $("#tempToggle").click(function(){
        
-       if(weatherInfo.type == "C"){
-           var far = (weatherInfo.far).toFixed(2);
+       if(weatherInfo.type === "C"){
+       var far = (weatherInfo.far).toFixed(2);
        $("#temp").html(far);
        weatherInfo.type = "F";
        $("#tempType").html(weatherInfo.type);
 
        }
-       else if(weatherInfo.type == "F"){
+       else if(weatherInfo.type === "F"){
 
         var cel = (weatherInfo.cel).toFixed(2);
         $("#temp").html(cel);
@@ -144,14 +173,15 @@ $(document).ready(function(){
 
        }
    });
+   
+   //Completes an arbitrary search for the weather in Central Park
    $("#NewYork").click(function(){
        var nyLat = 40.785091;
        var nyLong = -73.968285;
        getWeather(nyLong, nyLat);
         $("#city").html("New York City");
-        hour = hour + 3;
-        console.log(hour);
-        changeBGNY(hour);
+        nyHour = hour + 3;
+        changeBGNY(nyHour);
        
    });
    
@@ -160,7 +190,6 @@ $(document).ready(function(){
        var laLong = -118.2437;
        getWeather(laLong, laLat);
         $("#city").html("Los Angeles");
-        console.log(hour);
         changeBGLA(hour);
     });
    
@@ -169,11 +198,11 @@ $(document).ready(function(){
        var chiLong = -87.6298;
        getWeather(chiLong, chiLat);
         $("#city").html("Chicago");
-        hour = hour + 2;
-        console.log(hour);
-        changeBGChi(hour);
+        chiHour = hour + 2;
+        changeBGChi(chiHour);
     });
     
+    //returns back to the initial longitude and latitude
              $("#home").click(function(){
                  getWeather(long, lat);
     });
